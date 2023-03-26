b6aca9a87260e92888786d58644e08f03abc85d5
aleksanderwalczuk <ohleg@windowslive.com>
2023-03-26 11:12:59 +0200

    chore: add custom findOne to settings

--- src/api/settings/controllers/settings.ts
+++ src/api/settings/controllers/settings.ts
@@ -2,6 +2,19 @@
  * settings controller
  */
 
-import { factories } from '@strapi/strapi'
+import { GetAttributesValues, factories } from '@strapi/strapi'
+import { Context } from 'koa'
 
-export default factories.createCoreController('api::settings.settings');
+const key = "api::settings.settings";
+
+export default factories.createCoreController(key, ({ strapi }) => ({
+  async find (ctx: Context ) {
+    const entity: GetAttributesValues<"api::settings.settings"> = await strapi.entityService.findOne(key, 1, {
+      populate: ['home_page', 'navigation', 'contact', 'social'],
+    });
+
+    const sanitized: GetAttributesValues<typeof key> = this.sanitizeOutput(entity, ctx)
+
+    return sanitized;
+  },
+}));
