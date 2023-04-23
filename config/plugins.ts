fa05f146cc57eb6c53e1a5835a762306a4c428ee
aleksanderwalczuk <ohleg@windowslive.com>
2023-04-23 11:10:31 +0200

    feat: add strapi upload local plugin

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,9 +1,22 @@
 export default ({ env }) => ({
   sentry: {
-    enabled: env('NODE_ENV') === 'production',
+    enabled: env("NODE_ENV") === "production",
     config: {
-      dsn: env('SENTRY_DSN'),
+      dsn: env("SENTRY_DSN"),
       sendMetadata: true,
     },
   },
+  upload: {
+    config: {
+      provider: "local",
+      breakpoints: {
+        xlarge: 1920,
+        large: 1000,
+        medium: 750,
+        small: 500,
+        xsmall: 64
+      },
+      sizeLimit: 250 * 1024 * 1024, // 256mb in bytes
+    },
+  }
 });
