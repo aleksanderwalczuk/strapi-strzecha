9b5078474002f563a19aa23587e8aa08eb9bcdc1
aleksanderwalczuk <ohleg@windowslive.com>
2023-04-23 08:54:26 +0200

    chore: add sentry

---
+++ config/plugins.ts
@@ -0,0 +1,9 @@
+export default ({ env }) => ({
+  sentry: {
+    enabled: env('NODE_ENV') === 'production',
+    config: {
+      dsn: env('SENTRY_DSN'),
+      sendMetadata: true,
+    },
+  },
+});
