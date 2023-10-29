baaba306670874b7ea8bb66b48371eaaf1c95ba9
aleksanderwalczuk <ohleg@windowslive.com>
2023-10-29 14:36:55 +0100

    feat: add sitemap route

--- src/api/product/controllers/product.ts
+++ src/api/product/controllers/product.ts
@@ -4,4 +4,31 @@
 
 import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController("api::product.product");
+export default factories.createCoreController(
+  "api::product.product",
+  ({ strapi }) => ({
+    async meta(ctx) {
+      const limit = 100;
+      const page = ctx.query.start != null ? Number(ctx.query.start) : 0;
+      try {
+        const entries = await strapi.entityService.findMany(
+          "api::product.product",
+          {
+            fields: ["uid", "updatedAt"],
+            populate: ["images"],
+            publicationState: "live",
+            start: page !== 0 ? page * limit : 0,
+            limit: 100,
+          }
+        );
+
+        return {
+          data: entries,
+          pagination: { pageSize: limit, page: page + 1 },
+        };
+      } catch (error) {
+        return error;
+      }
+    },
+  })
+);
---
+++ src/api/product/routes/extra-product.ts
@@ -0,0 +1,9 @@
+export default {
+  routes: [
+    {
+      method: "GET",
+      path: "/product/meta",
+      handler: "product.meta",
+    },
+  ],
+};
