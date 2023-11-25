ea96a66390f29cfa80bffa5241f2fdbaaafb4288
aleksanderwalczuk <ohleg@windowslive.com>
2023-11-25 02:00:26 +0100

    feat: add limit param to ig endpoint

--- src/api/ig/controllers/ig.ts
+++ src/api/ig/controllers/ig.ts
@@ -7,11 +7,11 @@ import axios from "axios";
 import { Context } from "koa";
 import { createTokenEntry, getToken } from "../../../utils/ig";
 
-export default factories.createCoreController('api::ig.ig', ({ strapi }) => ({
+export default factories.createCoreController("api::ig.ig", ({ strapi }) => ({
   async find(ctx: Context) {
     const url = `${process.env.IG_API_URL}/me/media`;
 
-    let { before, after } = ctx.query;
+    let { before, after, limit } = ctx.query;
 
     let token = await getToken(strapi);
 
@@ -22,24 +22,28 @@ export default factories.createCoreController('api::ig.ig', ({ strapi }) => ({
 
     ctx.assert(token != null, 500, "Failed to retrieve ig token");
 
-    const fields = "id,media_type,thumbnail_url,permalink,username,timestamp"
+    const fields = "id,media_type,thumbnail_url,permalink,username,timestamp";
     const res = await axios.get(url, {
       method: "GET",
       params: {
         fields,
         access_token: token.access_token,
-        limit: 25,
-        ...(after != null ? {
-          after
-        }: {}),
-        ...(before != null ? {
-          before
-        }: {})
-      }
+        limit: limit ?? 10,
+        ...(after != null
+          ? {
+              after,
+            }
+          : {}),
+        ...(before != null
+          ? {
+              before,
+            }
+          : {}),
+      },
     });
 
     ctx.assert(res.data != null);
 
     return res.data;
   },
-}))
+}));
